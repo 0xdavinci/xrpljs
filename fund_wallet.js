4d23274acfa4f5966fc3f7200e2ee7a7d51106a3
♡Dᴀⱽɨ₦₵ɨ♡ <160800039+0xdavinci@users.noreply.github.com>
2025-07-29 18:40:25 -0400

    fund wallet

---
+++ fund_wallet.js
@@ -0,0 +1,5 @@
+import xrpl from "xrpl";
+const api = new xrpl.Client("wss://s.altnet.rippletest.net:51233");
+await api.connect();
+const {wallet, balance} = await api.fundWallet();
+console.log(wallet,balance);
--- generate_wallet.js
+++ generate_wallet.js
@@ -2,4 +2,5 @@ import { Wallet } from "xrpl";
 
 const wallet = Wallet.generate();
 console.log(wallet);
-
+console.log(wallet.address);
+console.log(wallet.seed);
