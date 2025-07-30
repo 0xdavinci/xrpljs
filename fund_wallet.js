3701fc8752fa8e94795873f2981839b068624241
♡Dᴀⱽɨ₦₵ɨ♡ <160800039+0xdavinci@users.noreply.github.com>
2025-07-29 20:20:27 -0400

    add client disconnect

--- fund_wallet.js
+++ fund_wallet.js
@@ -1,5 +1,6 @@
 import xrpl from "xrpl";
 const api = new xrpl.Client("wss://s.altnet.rippletest.net:51233");
 await api.connect();
-const {wallet, balance} = await api.fundWallet();
-console.log(wallet,balance);
+const { wallet, balance } = await api.fundWallet();
+console.log(wallet, balance);
+await api.disconnect();
